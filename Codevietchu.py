b5552ac38ed1c23ca19d2eb3c6c6769b4e810a86
Long12102007 <huynhngocbaolong1210@gmail.com>
2025-10-03 11:35:06 +0700

    Add files via upload

---
+++ Codevietchu.py
@@ -0,0 +1,10 @@
+import time
+text = " hello bé iu của anh"
+tex1= " anh yêu béeee :> <3 "
+for char in text:
+     print(char,end="",flush=True)
+     time.sleep(0.1)
+print()
+for char in tex1:
+    print(char,end="" ,flush=True) 
+    time.sleep(0.1)
---
+++ ranomwheels.py
@@ -0,0 +1,6 @@
+import random
+i = random.randint(0,100)
+if i > 50:
+  print("heads")
+elif i < 50:
+  print("tails") 
